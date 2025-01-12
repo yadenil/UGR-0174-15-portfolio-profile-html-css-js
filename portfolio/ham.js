9f37b6a48157f22898e090e864018d2356ca3d5c
yadenil <163213810+yadenil@users.noreply.github.com>
2025-01-12 18:28:24 +0300

    Delete portfolio directory

--- portfolio/ham.js
+++
@@ -1,8 +0,0 @@
-// Get DOM elements
-const menuToggle = document.getElementById("menu-toggle");
-const navLinks = document.getElementById("navbar");
-
-// Toggle the active class
-menuToggle.addEventListener("click", () => {
-  navLinks.classList.toggle("active");
-});
